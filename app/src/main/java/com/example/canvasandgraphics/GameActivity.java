fe41b6dbeb7f69524d50d320b70aa64cc94111e7
yishay33 <yishiehoffman@gmail.com>
2024-11-05 15:52:45 +0200

    first

---
+++ app/src/main/java/com/example/canvasandgraphics/GameActivity.java
@@ -0,0 +1,67 @@
+package com.example.canvasandgraphics;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameActivity extends AppCompatActivity {
+
+    MyView myView;
+    Thread thread;
+    boolean userAskBack = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myView = new MyView(this);
+        thread = new Thread(myView);
+        thread.start();
+
+        setContentView(myView);
+
+    }
+    @Override
+    public void finish() {
+        super.finish();
+        userAskBack = true;
+        myView.threadRunning = false;
+        while(true){
+            try {
+                thread.join();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            break;
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(myView !=null){
+            myView.resume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("data", "pause");
+        if(userAskBack){
+            Log.d("data", "user ask back");
+        }
+        else if(myView != null){
+            myView.pause();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/canvasandgraphics/MyView.java
@@ -0,0 +1,72 @@
+package com.example.canvasandgraphics;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class MyView extends SurfaceView implements Runnable {
+
+    SurfaceHolder holder;
+    boolean threadRunning = true;
+    boolean isRunning = true;
+    Canvas c;
+    Paint p;
+    private boolean firstTime = true;
+
+
+    public MyView(Context context) {
+        super(context);
+        this.holder = getHolder();
+    }
+
+    @Override
+    public void run() {
+        while (threadRunning) {
+            if (isRunning) {
+                if (!holder.getSurface().isValid())
+                    continue;
+                c = null;
+
+                try {
+                    c = this.getHolder().lockCanvas();
+                    synchronized (this.getHolder()) {
+                        c.drawColor(Color.CYAN);
+
+                    }
+
+                } catch (Exception e) {
+                } finally {
+                    if (c != null) {
+                        this.getHolder().unlockCanvasAndPost(c);
+
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //code to execute when touch is detected
+        return true;
+    }
+
+    public void pause() {
+        isRunning = false;
+    }
+
+    public void resume() {
+        isRunning = true;
+    }
+
+    public void destroy() {
+        isRunning = false;
+        ((GameActivity) getContext()).finish();
+    }
+}
